73ca82ab88dca3f85ad5f93f25377e54511044dd
shivam <shivam@spectertech.io>
2025-04-21 19:46:35 +0530

    Changing to check dynamic pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-// import {User} from './pages/Home';
+import User from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/user/:id" element={<User />} /> */}
+        <Route path="/user/:id" element={<User />} />
       </Routes>
     </BrowserRouter>
   );
